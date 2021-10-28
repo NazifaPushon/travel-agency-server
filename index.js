efabf7a6d54db1be4e5c92575cfb82f388bdd1d4
NazifaPushon <mashiat.pushon@gmail.com>
2021-10-29 04:22:20 +0600

    basic setup

--- index.js
+++ index.js
@@ -16,6 +16,75 @@ console.log(uri)
 async function run() {
     try {
         await client.connect();
+        const database = client.db("travel-agency")
+        const tourCollection = database.collection("destinations")
+        const bookingCollection = database.collection('bookings')
+
+        //Get All Data Api
+        app.get('/tours' , async(req ,res) => {
+            const cursor = tourCollection.find({})
+            const result = await cursor.toArray()
+            res.json(result)
+        })
+
+        // get single data api
+        app.get('/singleTour/:id' , async(req , res) => {
+            const id = req.params.id;
+            const query = {_id : ObjectId(id)}
+            const result = await tourCollection.findOne(query)
+            res.json(result)
+        })
+
+        //Post Tours 
+        app.post('/tourPost',async(req ,res) => {
+            const newTour = req.body;
+            const result = await tourCollection.insertOne(newTour);
+            res.json(result)
+        })
+
+        //get my bookings 
+        app.get('/myBookings/:email' , async(req ,res) => {
+            const email = req.params.email;
+            const result = await bookingCollection.find({email:email}).toArray()
+            res.json(result);
+        })
+
+        //get all bookings api
+        app.get('/bookings' , async(req ,res) => {
+            const cursor = bookingCollection.find({})
+            const result = await cursor.toArray()
+            res.json(result)
+        })
+
+        //Post a booking api
+        app.post('/postBooking' ,async(req ,res) => {
+            const booking = req.body;
+            const result = await bookingCollection.insertOne(booking)
+            res.send(result)
+        })
+
+        //delete booking api 
+        app.delete('/deleteBooking/:id' , async(req , res) => {
+            const id = req.params.id;
+            const query = {_id:ObjectId(id)}
+            const result = await bookingCollection.deleteOne(query)
+            res.json(result)
+        })
+
+        //update booking api 
+        app.put('/updateStatusBooking/:id' , async(req , res) => {
+            const id = req.params.id;
+            const updatedBooking = req.body;
+            const query = {_id:ObjectId(id)};
+            const options = { upsert : true}
+            const updatedDoc = {
+                $set: {  
+                  status:updatedBooking.status
+                },
+            };
+            const result =await usersCollection.updateOne(query,updatedDoc,options)
+            res.json(result)
+        })
         console.log("database is connected")
     } finally {
 
